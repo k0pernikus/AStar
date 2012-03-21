0e9577b96b7d5400eb021c468c73451c5f99feba
Jesper Persson <JesperT.Persson@gmail.com>
2012-03-21 16:44:42 +0100

    Removed last (known) error from algorithm

--- src/AStar.java
+++ src/AStar.java
@@ -6,63 +6,57 @@ public class AStar {
 	private List <SmartButton> openList;
 	private SmartButton currentTile;
 
+	
+	/*Main algorithm. Given a start and a goal, will return the shortest path between the two.*/
 	public List<SmartButton> getPath(SmartButton start, SmartButton stop){
-
 		openList = new ArrayList <SmartButton>();
 		ArrayList <SmartButton> path = new ArrayList<SmartButton>();
 		openList.add(start);
-		currentTile = start;
-		System.out.println("openListsize: " + openList.size());
-
+		
 		while(true){
-			//Loop through the open list to see if we've added the goal-tile.
-			int pointer = 0;
-			double currentF = currentTile.f;
-			for (int i=0;i<openList.size();i++){
-				if (openList.get(i).f < currentF){
-					pointer=i;
-					currentF=openList.get(i).f;
-				}
-				
-				if(openList.get(pointer).equals(stop)){
-					path.add(openList.get(i));
-					currentTile = path.get(0);
+			int pointer = findBestTile();
+			currentTile=openList.get(pointer);
+				if(currentTile.equals(stop)){
+					path.add(currentTile);
 					while(currentTile!=start){
 						path.add(currentTile.parent);
 						currentTile = currentTile.parent;
 					}
-					System.out.println(""+ path.size());
 					return path;
 					
 					//The goal was not found in the openList
 				}else {
-					
-					//Adds the currentTile to the close list, and loops through the open list for the SmartButton with the lowest f-value, selecting this as the 
-					// new currentTile
-					currentF=openList.get(0).f;
-					for (i=0;i<openList.size();i++){
-						if (currentF >openList.get(i).f && openList.get(i).isOpen()){
-							currentF=openList.get(i).f;
-							pointer=i;
-						}
-					}
+					updateNeighbors(currentTile.getNeighbours());
 					currentTile.setClosed(true);
 					currentTile.setOpen(false);
 					if (openList.size() >1){
 						removeFromOpen(currentTile);
 					}
 					currentTile = openList.get(pointer);
-					
-					updateNeighbors(currentTile.getNeighbours());
 			
 				}
 			}
 
 		}
 
+	/*Returns the index of the tile in the open list with the lowest f-value*/
+	private int findBestTile(){
+		int pointer = 0;
+		double currentF = openList.get(0).f;
+		for (int i=0;i<openList.size();i++){
+			if (openList.get(i).f < currentF){
+				pointer=i;
+				currentF=openList.get(i).f;
+			}
+		}
+		return pointer;
 	}
 	
-	/*Given a list of SmartButtons representing all neighbors of some other SmartButton will update each of these neighbors */
+	/*Loop through the given list, and updates depending on its relation to its adjacent tile, the one currently selected
+	 * Should a tile be in a closed or open state, the length of the path between it and the start
+	 * will be compared to what it would have been had we measured the current path to it instead. Should the tile be neither closed nor open, 
+	 * the path to it will be updated, and it will be added to the open list for consideration
+	 * */
 	private void updateNeighbors(List <SmartButton> currentNeighbors){
 		for (int k=0;k<currentNeighbors.size();k++){
 
@@ -70,13 +64,13 @@ public class AStar {
 			// currentTile
 			if (currentNeighbors.get(k).isClosed() && currentPathIsShorter(currentNeighbors.get(k))){
 				currentNeighbors.get(k).g=(currentTile.isDiagonal(currentNeighbors.get(k)) ? 
-						currentTile.g+Math.sqrt(2) : currentTile.g+1);
+						currentTile.g+1.4 : currentTile.g+1);
 				currentNeighbors.get(k).parent=currentTile;
 
 				//if a tile is open and the current paths g-value would be lower than its old g-value, we update the tiles g-value and sets it parent to 
 				// currentTile
 			}else if (currentNeighbors.get(k).isOpen() && currentPathIsShorter(currentNeighbors.get(k))) {
-				currentNeighbors.get(k).g=(currentTile.isDiagonal(currentNeighbors.get(k)) ? currentTile.g+Math.sqrt(2) : currentTile.g+1);
+				currentNeighbors.get(k).g=(currentTile.isDiagonal(currentNeighbors.get(k)) ? currentTile.g+1.4: currentTile.g+1);
 				currentNeighbors.get(k).parent=currentTile;
 
 				//if a tile is neither open nor closed, we add it the openList and update the open-value accordingly.
@@ -87,6 +81,7 @@ public class AStar {
 					currentNeighbors.get(k).setOpen(true);
 					openList.add(currentNeighbors.get(k));
 					currentNeighbors.get(k).parent=currentTile;
+					currentNeighbors.get(k).g=(currentTile.isDiagonal(currentNeighbors.get(k)) ? currentTile.g+1.4: currentTile.g+1);
 				}
 			}
 		}
